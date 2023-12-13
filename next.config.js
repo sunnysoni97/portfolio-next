237329a5468fb021b74af23ff682e3261e2fa4be
sunnysoni <sunnysoni97@gmail.com>
2023-12-13 21:28:12 +0100

    changed asset prefix (removed extra /)

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV == "production";
 const nextConfig = {
   basePath: isProd ? "/portfolio-next" : "",
   output: "export",
-  assetPrefix: isProd ? "/portfolio-next/" : "",
+  assetPrefix: isProd ? "/portfolio-next" : "",
 };
 
 module.exports = nextConfig;
