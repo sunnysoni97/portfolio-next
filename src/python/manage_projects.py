d86d062af8622f944e91c5b85be3517f87119f95
sunnysoni <sunnysoni97@gmail.com>
2023-12-20 23:18:56 +0100

    extended python script to : milestones slider + timeline data

--- src/python/manage_projects.py
+++ src/python/manage_projects.py
@@ -3,10 +3,7 @@ import os
 import json
 from typing import List, Callable
 import sys
-
-JSON_KEYS = ["title", "desc", "imgUrl", "repoUrl", "highlight"]
-FILE_PATH = Path(os.path.dirname(__file__)).parent
-FILE_PATH = FILE_PATH.joinpath("app", "projects", "_posts", "project_list.json")
+import argparse
 
 
 def create_or_read_list(json_path: Path) -> List[dict]:
@@ -22,9 +19,9 @@ def create_or_read_list(json_path: Path) -> List[dict]:
     return json_data
 
 
-def write_list(json_obj: List[dict]) -> bool:
+def write_list(json_obj: List[dict], file_path: Path) -> bool:
     try:
-        with open(FILE_PATH, "wt") as f:
+        with open(file_path, "wt") as f:
             json.dump(json_obj, f)
         return True
     except Exception as ex:
@@ -44,21 +41,22 @@ def handle_menu(menu_options, menu_functions) -> Callable:
         return handle_menu(menu_options=menu_options, menu_functions=menu_functions)
 
 
-def add_project(json_data: List[dict]) -> None:
+def add_project(json_data: List[dict], json_keys: List[str], file_path: Path) -> None:
     try:
         new_project = {}
-        for key in JSON_KEYS:
+        for key in json_keys:
             new_project[key] = input(f"{key} : ")
-        new_project["highlight"] = json.loads(new_project["highlight"].lower())
+        if "highlight" in json_keys:
+            new_project["highlight"] = json.loads(new_project["highlight"].lower())
         json_data.append(new_project)
-        write_list(json_obj=json_data)
+        write_list(json_obj=json_data, file_path=file_path)
     except Exception as ex:
         print(f"failed to add : {ex}")
     finally:
         return
 
 
-def view_list(json_data: List[dict]) -> None:
+def view_list(json_data: List[dict], json_keys: List[str], file_path: Path) -> None:
     print("List of Projects : ")
     i = 0
     for proj in json_data:
@@ -67,28 +65,29 @@ def view_list(json_data: List[dict]) -> None:
     return
 
 
-def edit_project(json_data: List[dict]):
+def edit_project(json_data: List[dict], json_keys: List[str], file_path: Path) -> None:
     try:
         proj_n = int(input("Select project to edit (n): "))
         if proj_n >= 0 and proj_n < len(json_data):
             old_proj = json_data[proj_n]
             print(f"Old details : {old_proj}")
             print(f"Enter new details (leave the input key blank for no change) : ")
-            for key in JSON_KEYS:
+            for key in json_keys:
                 new_key = input(f"{key} : ")
                 if len(new_key) > 0:
                     json_data[proj_n][key] = new_key
-            json_data[proj_n]["highlight"] = json.loads(
-                json_data[proj_n]["highlight"].lower()
-            )
-            write_list(json_obj=json_data)
+            if "highlight" in json_keys:
+                json_data[proj_n]["highlight"] = json.loads(
+                    json_data[proj_n]["highlight"].lower()
+                )
+            write_list(json_obj=json_data, file_path=file_path)
     except Exception as ex:
         print(f"failed to edit : {ex}")
     finally:
         return
 
 
-def reorder_list(json_data: List[dict]):
+def reorder_list(json_data: List[dict], json_keys: List[str], file_path: Path) -> None:
     try:
         proj_n = int(input("Select project number to reorder : "))
         if proj_n >= 0 and proj_n < len(json_data):
@@ -96,28 +95,28 @@ def reorder_list(json_data: List[dict]):
             if new_n >= 0 and new_n < len(json_data) and new_n != proj_n:
                 proj = json_data.pop(proj_n)
                 json_data.insert(new_n, proj)
-                write_list(json_obj=json_data)
+                write_list(json_obj=json_data, file_path=file_path)
     except Exception as ex:
         print(f"failed to reorder : {ex}")
     finally:
         return
 
 
-def del_project(json_data: List[dict]):
+def del_project(json_data: List[dict], json_keys: List[str], file_path: Path) -> None:
     try:
         proj_n = int(input("Select project number to delete : "))
         if proj_n >= 0 and proj_n < len(json_data):
             confirm = input("Are you sure ? (y/n) : ")
             if confirm == "y" or confirm == "Y":
                 json_data.pop(proj_n)
-                write_list(json_obj=json_data)
+                write_list(json_obj=json_data, file_path=file_path)
     except Exception as ex:
         print(f"failed to delete : {ex}")
     finally:
         return
 
 
-def exit(json_data: List[dict]):
+def exit(json_data: List[dict], json_keys: List[str], file_path: Path) -> None:
     print("exiting program")
     sys.exit(0)
 
@@ -125,9 +124,37 @@ def exit(json_data: List[dict]):
 MENU_OPTS = ["Add", "View", "Edit", "Reorder", "Delete", "Exit"]
 MENU_FUNCS = [add_project, view_list, edit_project, reorder_list, del_project, exit]
 
+FILE_DIR = Path(os.path.dirname(__file__)).parent
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--target", type=str, default="projects")
+
+
 if __name__ == "__main__":
-    json_data = create_or_read_list(json_path=FILE_PATH)
+    args = parser.parse_args()
+    tgt = args.target
+
+    tgt_list = ["projects", "milestones_timeline", "milestones_slider"]
+
+    assert tgt in tgt_list, f"target json incorrect. choose one from : {tgt_list}"
+
+    tgt_idx = tgt_list.index(tgt)
+    tgt_paths = [
+        Path("projects", "_posts", "project_list.json"),
+        Path("milestones", "_posts", "timeline_data.json"),
+        Path("milestones", "_posts", "slider_data.json"),
+    ]
+    tgt_json_keys = [
+        ["title", "desc", "imgUrl", "repoUrl", "highlight"],
+        ["year", "title", "desc"],
+        ["year", "title", "desc", "imgUrl"],
+    ]
+
+    json_keys = tgt_json_keys[tgt_idx]
+    file_path = Path.joinpath(FILE_DIR, "app", tgt_paths[tgt_idx])
+
+    json_data = create_or_read_list(json_path=file_path)
 
     while True:
         func = handle_menu(menu_options=MENU_OPTS, menu_functions=MENU_FUNCS)
-        func(json_data=json_data)
+        func(json_data=json_data, json_keys=json_keys, file_path=file_path)
