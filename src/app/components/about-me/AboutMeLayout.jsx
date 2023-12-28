e396e7e2b2c116359d178a8054c464a7f3821fd5
sunnysoni <sunnysoni97@gmail.com>
2023-12-28 20:00:33 +0100

    added bg images (about-me)

--- src/app/components/about-me/AboutMeLayout.jsx
+++ src/app/components/about-me/AboutMeLayout.jsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { SimpleGrid, ScrollArea, Text, Image, Divider } from "@mantine/core";
+import {
+  SimpleGrid,
+  ScrollArea,
+  Text,
+  Image,
+  Divider,
+  Paper,
+} from "@mantine/core";
 
 import Education from "@/app/components/about-me/Education";
 import WorkEx from "@/app/components/about-me/WorkEx";
@@ -14,11 +21,11 @@ import CheckMobile from "@/app/components/common/CheckMobile";
 const AboutMeLayout = (props) => {
   const isMobile = CheckMobile();
   const divList = [
-    ["Education", "test_bg.png", Education],
-    ["Work Experience", "test_bg.png", WorkEx],
-    ["Tech Knowledge", "test_bg.png", TechKnowledge],
-    ["Research Interests", "test_bg.png", ResearchInterests],
-    ["Hobbies", "test_bg.png", Hobbies],
+    ["Education", "education.png", Education],
+    ["Work Experience", "experience.png", WorkEx],
+    ["Tech Knowledge", "tech.png", TechKnowledge],
+    ["Research Interests", "research.png", ResearchInterests],
+    ["Hobbies", "hobbies.png", Hobbies],
   ];
 
   return (
@@ -33,21 +40,28 @@ const AboutMeLayout = (props) => {
         const bgImage = value[1];
         const DataElement = value[2];
         return (
-          <ScrollArea key={index} h={400} maw={650} type="hover">
+          <ScrollArea
+            key={index}
+            h={400}
+            maw={650}
+            type="hover"
+            classNames={{ root: "shadow-md rounded-lg" }}
+            p="md"
+          >
             <Image
-              w="200px"
-              src={`${basePath}/images/${bgImage}`}
+              w="100px"
+              src={`${basePath}/images/about-me/bg/${bgImage}`}
               style={{
                 zIndex: -1,
-                filter: "blur(1px) opacity(30%)",
+                filter: "blur(0.5px) opacity(30%)",
                 position: "absolute",
                 right: "1em",
-                top: "2em",
+                top: "5em",
               }}
               h="auto"
               alt=""
             />
-            <Text className="!text-lg !font-bold">{title}</Text>
+            <Text className="!text-xl !font-bold">{title}</Text>
             <Divider my="md" />
             <DataElement />
           </ScrollArea>
