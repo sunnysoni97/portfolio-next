0e144517244ee3d25b7dd2c92c1ee2e0d13016a4
sunnysoni <sunnysoni97@gmail.com>
2023-12-28 18:54:30 +0100

    fixed about-me background positioning

--- src/app/components/about-me/AboutMeLayout.jsx
+++ src/app/components/about-me/AboutMeLayout.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SimpleGrid, ScrollArea, Text, Image } from "@mantine/core";
+import { SimpleGrid, ScrollArea, Text, Image, Divider } from "@mantine/core";
 
 import Education from "@/app/components/about-me/Education";
 import WorkEx from "@/app/components/about-me/WorkEx";
@@ -9,8 +9,10 @@ import ResearchInterests from "@/app/components/about-me/ResearchInterests";
 import Hobbies from "@/app/components/about-me/Hobbies";
 
 import { basePath } from "../../../../next.config";
+import CheckMobile from "@/app/components/common/CheckMobile";
 
 const AboutMeLayout = (props) => {
+  const isMobile = CheckMobile();
   const divList = [
     ["Education", "test_bg.png", Education],
     ["Work Experience", "test_bg.png", WorkEx],
@@ -20,22 +22,33 @@ const AboutMeLayout = (props) => {
   ];
 
   return (
-    <SimpleGrid cols={{ base: 1, md: 2, xl: 3 }}>
+    <SimpleGrid
+      cols={{ base: 1, lg: 2 }}
+      spacing="xl"
+      mx="auto"
+      w={isMobile ? "95%" : "90%"}
+    >
       {divList.map((value, index) => {
         const title = value[0];
         const bgImage = value[1];
         const DataElement = value[2];
         return (
-          <ScrollArea key={index} h={400} type="hover">
+          <ScrollArea key={index} h={400} maw={650} type="hover">
             <Image
               w="200px"
               src={`${basePath}/images/${bgImage}`}
-              className="fixed"
-              style={{ zIndex: -1, filter: "blur(1px) opacity(30%)" }}
+              style={{
+                zIndex: -1,
+                filter: "blur(1px) opacity(30%)",
+                position: "absolute",
+                right: "1em",
+                top: "2em",
+              }}
               h="auto"
               alt=""
             />
-            <Text>{title}</Text>
+            <Text className="!text-lg !font-bold">{title}</Text>
+            <Divider my="md" />
             <DataElement />
           </ScrollArea>
         );
