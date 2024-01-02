f2b73ccda5c68d2546738c76772d1da16a1e4da8
sunnysoni <sunnysoni97@gmail.com>
2024-01-02 07:51:41 +0100

    - moved projects page to layout format (standardised) - filled in contacts page

--- src/app/components/contact/ContactLayout.jsx
+++ src/app/components/contact/ContactLayout.jsx
@@ -1,20 +1,55 @@
 "use client";
 
-import { Flex, Paper, Text } from "@mantine/core";
+import { Flex, Paper, Text, Group, Button, Stack } from "@mantine/core";
+import { useRouter } from "next/navigation";
+
+import { BrandLinkedin, BrandGmail } from "tabler-icons-react";
 
 import ContactForm from "@/app/components/contact/ContactForm";
 
 const ContactLayout = () => {
+  const router = useRouter();
   return (
     <Flex
       direction={{ base: "column", xl: "row" }}
       columnGap="xl"
-      align={{ base: "center", xl: "flex-start" }}
+      align={{ base: "center", xl: "stretch" }}
       justify="space-evenly"
       wrap="nowrap"
     >
-      <Paper w={{ base: "90%", md: "60%", xl: "50%" }} mx="auto" shadow="lg">
-        <Text className="!text-lg">{"This is the links area."}</Text>
+      <Paper
+        w={{ base: "90%", md: "60%", xl: "50%" }}
+        mx="auto"
+        shadow="lg"
+        p="lg"
+      >
+        <Stack justify="center" h="100%" gap="xl">
+          <Text
+            className="text-justify"
+            style={{ whiteSpace: "pre-wrap" }}
+          >{`If you would like to get in touch with me, feel free to reach out to me through any of my socials provided below. You can also use the contact form. I am always in for a quick exchange on everything interesting in the exciting world of AI and computers. In the end, we might end up getting to know some undiscovered facts from one another, which is always a pleasurable moment.\n\nThanks for taking your time to go through my portfolio. I bid you a good day!`}</Text>
+
+          <Group my="lg" justify="center" gap="xl">
+            <Button
+              variant="transparent"
+              size="compact-xl"
+              onClick={() => {
+                router.push("https://www.linkedin.com/in/sunnysoni97/");
+              }}
+            >
+              <BrandLinkedin size="48" strokeWidth={1.5} color="blue" />
+            </Button>
+            <Button
+              variant="transparent"
+              size="compact-xl"
+              onClick={() => {
+                router.push("mailto:sunnysoni97@gmail.com");
+              }}
+            >
+              <BrandGmail size="48" strokeWidth={1.5} color="red" />
+            </Button>
+          </Group>
+        </Stack>
       </Paper>
       <Paper w={{ base: "90%", md: "60%", xl: "50%" }} mx="auto" shadow="lg">
         <ContactForm />
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,39 +1,9 @@
-import Link from "next/link";
-
-import { Text } from "@mantine/core";
-
-import ProjectGallery from "@/app/components/projects/ProjectGallery";
-import ProjectCarousel from "@/app/components/projects/ProjectCarousel";
+import ProjectsLayout from "@/app/components/projects/ProjectsLayout";
 
 export const metadata = {
   title: "Projects",
 };
 
 export default function Projects() {
-  return (
-    <>
-      <Text>
-        {
-          "On this page, you can find a list of some of the interesting works I have been a part of. These works are available publicly through my "
-        }
-        <Link
-          href="https://github.com/sunnysoni97"
-          className="text-blue-500 no-underline"
-        >
-          {"GitHub profile"}
-        </Link>
-        {
-          ". If some work catches your eye, feel free to check it out in details."
-        }
-      </Text>
-      <br />
-      <h2 className="text-xl font-bold">{"Spotlight"}</h2>
-      <br />
-      <ProjectCarousel />
-      <br />
-      <h2 className="text-xl font-bold">{"List of Projects"}</h2>
-      <br />
-      <ProjectGallery />
-    </>
-  );
+  return <ProjectsLayout />;
 }
